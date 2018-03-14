fe116d102f6924e7134235c0a064f5a00e1a7871
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-14 12:40:13 -0400

    Code Smell 4 - Long Method

--- backpack.rb
+++ backpack.rb
@@ -15,33 +15,15 @@ class Backpack
     day_of_week = @attributes[:day_of_week]
 
     # Ensure appropriate clothing is added to backpack
-    if weather == 'rainy'
-      @items << 'pants'
-      @items << 'shirt'
-      @items << 'umbrella'
-    elsif weather == 'cold'
-      @items << 'pants'
-      @items << 'shirt'
-      @items << 'jacket'
-    else
-      @items << 'pants'
-      @items << 'shirt'
-    end
+    add_clothing(weather)
 
     # Ensure gym shoes are added to backpack if it's a gym day
     # Gotta get to the gym on Mondays and Thursdays.
-    if day_of_week == 'monday' || day_of_week == 'thursday'
-      #add gym shoes to items
-      @items << 'gym shoes'
-    end
+    add_gym_shoes(day_of_week)
 
     # Bring a packed lunch on all weekdays
-    if day_of_week != 'saturday' && day_of_week != 'sunday'
-      @items << 'packed lunch'
-    elsif false
-      # Used to bring snacks on weekend trips, but now I just buy 'em
-      @items << 'snacks'
-    end
+    add_lunch(day_of_week)
+
   end
 
   # Prints a summary packing list for Melinda's backpack
@@ -55,6 +37,33 @@ class Backpack
       output << "- #{item}"
     end
     output.join("\n")
+  end
+
+  def add_clothing(weather)
+    @items << 'pants'
+    @items << 'shirt'
+
+    if weather == 'rainy'
+      @items << 'umbrella'
+    elsif weather == 'cold'
+      @items << 'jacket'
+    end
+  end
+
+  def add_gym_shoes(day_of_week)
+    if day_of_week == 'monday' || day_of_week == 'thursday'
+      #add gym shoes to items
+      @items << 'gym shoes'
+    end
+  end
+
+  def add_lunch(day_of_week)
+    if day_of_week != 'saturday' && day_of_week != 'sunday'
+      @items << 'packed lunch'
+    elsif false
+      # Used to bring snacks on weekend trips, but now I just buy 'em
+      @items << 'snacks'
+    end
   end
 
 end
